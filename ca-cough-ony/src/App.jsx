ec3c557f8a843e5947a97084886cbde86034cc24
Hisham Bhatti <hishamb@cs.washington.edu>
2025-10-14 16:07:52 -0700

    Added page 3

--- ca-cough-ony/src/App.jsx
+++ ca-cough-ony/src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react"
 import Landing from "./Landing"
 import { useState } from "react"
+import About from "./About"
 
 export default function App() {
 
-  const [pageNum, setPageNum] = useState(1)
+  const [pageNum, setPageNum] = useState(3)
 
   return (
     <>
       {pageNum === 1 && (
         <Landing />
+      )}
+      {pageNum === 3 && (
+        <About />
       )}
     </>
   )
