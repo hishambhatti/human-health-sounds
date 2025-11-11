92830c81473b69715245e7362470a1c3172d178e
Hisham Bhatti <hishamb@cs.washington.edu>
2025-11-10 17:03:57 -0800

    Removed the Based on Bird Sounds

--- ca-cough-ony/src/components/About.jsx
+++ ca-cough-ony/src/components/About.jsx
@@ -20,16 +20,7 @@ export default function About({ handleClickExplore }) {
       {/* Text Section */}
       <div className='max-w-3xl text-gray-700 mt-8 leading-relaxed text-[1.05rem]'>
         <p className='mb-4'>
-          Built by Hisham Bhatti at the Ubicomp Lab at the University of Washington. Based on{" "}
-            <a
-              href="https://experiments.withgoogle.com/ai/bird-sounds/view"
-              className="text-sky-600 hover:underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Bird Sounds
-            </a>{" "}
-          from Google Creative Lab. Thanks to{" "}
+          Built by Hisham Bhatti at the Ubicomp Lab at the University of Washington. Thanks to{" "}
             <a
               href="https://homes.cs.washington.edu/~zzhihan"
               className="text-sky-600 hover:underline"
