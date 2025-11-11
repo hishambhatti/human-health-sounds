d7696ad37e84c0500dec55111a92cad14b6b8397
Hisham Bhatti <hishamb@cs.washington.edu>
2025-11-10 17:14:16 -0800

    Changed the About links

--- ca-cough-ony/src/components/About.jsx
+++ ca-cough-ony/src/components/About.jsx
@@ -20,7 +20,33 @@ export default function About({ handleClickExplore }) {
       {/* Text Section */}
       <div className='max-w-3xl text-gray-700 mt-8 leading-relaxed text-[1.05rem]'>
         <p className='mb-4'>
-          Built by Hisham Bhatti at the Ubicomp Lab at the University of Washington. Thanks to{" "}
+          Built by {" "}
+            <a
+              href="https://www.linkedin.com/in/hisham-bhatti"
+              className="text-sky-600 hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Hisham Bhatti
+            </a>{" "}
+          in the {" "}
+            <a
+              href="https://ubicomplab.cs.washington.edu"
+              className="text-sky-600 hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ubiquitous Computing Lab
+            </a>{" "}
+          at the University of Washington {" "}
+            <a
+              href="https://www.cs.washington.edu"
+              className="text-sky-600 hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Paul G. Allen School of Computer Science & Engineering.
+            </a>{" "} Thanks to{" "}
             <a
               href="https://homes.cs.washington.edu/~zzhihan"
               className="text-sky-600 hover:underline"
